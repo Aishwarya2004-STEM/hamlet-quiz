e5f0e4d7e3783a89bf20b834d4a5d5e2609fbc2b
snaeha <snaeha.shriram@gmail.com>
2022-02-17 12:33:20 -0600

    final commit

--- index.js
+++ index.js
@@ -1,48 +1,250 @@
-var char1 = 0;
-var char2 = 0;
-var char3 = 0;
-var char4 = 0;
-var char5 = 0;
+const resultsEl = document.getElementById("results");
+const homeBtn = document.getElementById("home-btn");
+const displayResultsEl = document.getElementById("display-results");
 
-// above are the variables that store the amount each character related answer has been picked
 
-function char1Add() {
-    char1 = char1 + 1;
+function q1a1() {
+	localStorage.setItem("q1", 1);
+	console.log(localStorage);
 }
 
-function char2Add() {
-    char2 = char2 + 1;
-
+function q1a2() {
+	localStorage.setItem("q1", 2);
+	console.log(localStorage);
 }
 
-function char3Add() {
-    char3 = char3 + 1;
-
+function q1a3() {
+	localStorage.setItem("q1", 3);
+	console.log(localStorage);
 }
 
-function char4Add() {
-    char4 = char4 + 1;
-
+function q1a4() {
+	localStorage.setItem("q1", 4);
+	console.log(localStorage);
 }
 
-function char5Add() {
-    char5 = char5 + 1;
-
-}
-// above are the functions that define the function for each character that add a count to the counter with the user input
-
-function finalResults() {
-    char1 = char1 / 6 * 100
-    char2 = char2 / 6 * 100
-    char3 = char3 / 6 * 100
-    char4 = char4 / 6 * 100
-    char5 = char5 / 6 * 100
-
-
-    var resultText = document.getElementById("results")
-    resultText.innerHTML = "You are " + char1 + "% char1" + "<br>You are " + char2 + "% char2" +
-        "<br>You are " + char3 + "% char3" + "<br>You are " + char4 + "% char4" + "<br>You are " + char5 + "% char5";
-
+function q2a1() {
+	localStorage.setItem("q2", 1);
+	console.log(localStorage);
 }
 
-// above is the function that defines the final calculations and to output results
+function q2a2() {
+	localStorage.setItem("q2", 2);
+	console.log(localStorage);
+}
+
+function q2a3() {
+	localStorage.setItem("q2", 3);
+	console.log(localStorage);
+}
+
+function q2a4() {
+	localStorage.setItem("q2", 4);
+	console.log(localStorage);
+}
+
+function q3a1() {
+	localStorage.setItem("q3", 1);
+	console.log(localStorage);
+}
+
+function q3a2() {
+	localStorage.setItem("q3", 2);
+	console.log(localStorage);
+}
+
+function q3a3() {
+	localStorage.setItem("q3", 3);
+	console.log(localStorage);
+}
+
+function q3a4() {
+	localStorage.setItem("q3", 4);
+	console.log(localStorage);
+}
+
+function q4a1() {
+	localStorage.setItem("q4", 1);
+	console.log(localStorage);
+}
+
+function q4a2() {
+	localStorage.setItem("q4", 2);
+	console.log(localStorage);
+}
+
+function q4a3() {
+	localStorage.setItem("q4", 3);
+	console.log(localStorage);
+}
+
+function q4a4() {
+	localStorage.setItem("q4", 4);
+	console.log(localStorage);
+}
+
+function q5a1() {
+	localStorage.setItem("q5", 1);
+	console.log(localStorage);
+}
+
+function q5a2() {
+	localStorage.setItem("q5", 2);
+	console.log(localStorage);
+}
+
+function q5a3() {
+	localStorage.setItem("q5", 3);
+	console.log(localStorage);
+}
+
+function q5a4() {
+	localStorage.setItem("q5", 4);
+	console.log(localStorage);
+}
+
+function q6a1() {
+	localStorage.setItem("q6", 1);
+	console.log(localStorage);
+}
+
+function q6a2() {
+	localStorage.setItem("q6", 2);
+	console.log(localStorage);
+}
+
+function q6a3() {
+	localStorage.setItem("q6", 3);
+	console.log(localStorage);
+}
+
+function q6a4() {
+	localStorage.setItem("q6", 4);
+	console.log(localStorage);
+}
+
+function q7a1() {
+	localStorage.setItem("q7", 1);
+	console.log(localStorage);
+}
+
+function q7a2() {
+	localStorage.setItem("q7", 2);
+	console.log(localStorage);
+}
+
+function q7a3() {
+	localStorage.setItem("q7", 3);
+	console.log(localStorage);
+}
+
+function q7a4() {
+	localStorage.setItem("q7", 4);
+	console.log(localStorage);
+}
+
+function q8a1() {
+	localStorage.setItem("q8", 1);
+	console.log(localStorage);
+}
+
+function q8a2() {
+	localStorage.setItem("q8", 2);
+	console.log(localStorage);
+}
+
+function q8a3() {
+	localStorage.setItem("q8", 3);
+	console.log(localStorage);
+}
+
+function q8a4() {
+	localStorage.setItem("q8", 4);
+	console.log(localStorage);
+}
+
+function q9a1() {
+	localStorage.setItem("q9", 1);
+	console.log(localStorage);
+}
+
+function q9a2() {
+	localStorage.setItem("q9", 2);
+	console.log(localStorage);
+}
+
+function q9a3() {
+	localStorage.setItem("q9", 3);
+	console.log(localStorage);
+}
+
+function q9a4() {
+	localStorage.setItem("q9", 4);
+	console.log(localStorage);
+}
+
+function q10a1() {
+	localStorage.setItem("q10", 1);
+	console.log(localStorage);
+}
+
+function q10a2() {
+	localStorage.setItem("q10", 2);
+	console.log(localStorage);
+}
+
+function q10a3() {
+	localStorage.setItem("q10", 3);
+	console.log(localStorage);
+}
+
+function q10a4() {
+	localStorage.setItem("q10", 4);
+	console.log(localStorage);
+}
+
+console.log(localStorage);
+
+
+
+resultsEl.addEventListener("click", function() {
+	values = Object.values(localStorage);
+	console.log(values);
+
+	var oneCount = 0;
+	var twoCount = 0;
+	var threeCount = 0;
+	var fourCount = 0;
+
+
+	for (i = values.length - 10; i <= values.length - 1; i++) {
+		if (values[i] == 1) {
+			oneCount += 1;
+		}
+		if (values[i] == 2) {
+			twoCount += 1;
+		}
+		if (values[i] == 3) {
+			threeCount += 1;
+		}
+		if (values[i] == 4) {
+			fourCount += 1;
+		}
+}
+		var charNum = Math.max(oneCount, twoCount, threeCount, fourCount);
+
+		if (oneCount == charNum) {
+			displayResultsEl.innerHTML = "You are Hamlet!"
+		} else if (twoCount == charNum) {
+			displayResultsEl.innerHTML = "You are Claudius!"
+		} else if (threeCount == charNum) {
+			displayResultsEl.innerHTML = "You are Ophelia!"
+		} else if (fourCount == charNum) {
+			displayResultsEl.innerHTML = "You are Polonius!"
+		}
+
+});
+
+homeBtn.addEventListener("click", function () {
+	localStorage.clear()});
+
